9b90a5e780f4db9290bf286f6e8a553f59a63632
Marina-Thigas <maomenosetec@gmail.com>
2026-04-28 08:42:52 -0300

    Criação do projeto pw3-ingresso

---
+++ src/main/java/br/com/etechoracio/ingresso/entity/Filme.java
@@ -0,0 +1,56 @@
+package br.com.etechoracio.ingresso.entity;
+
+import br.com.etechoracio.ingresso.enums.CategoriaFilmeEnum;
+import br.com.etechoracio.ingresso.enums.ClassificacaoIndicativaEnum;
+import br.com.etechoracio.ingresso.enums.SimNaoEnum;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.Duration;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "TBL_FILME")
+public class Filme {
+    @Id
+    @Column(name = "ID_FILME")
+    private Long id;
+
+    @Column(name = "TX_NOME")
+    private String nome;
+
+    @Column(name = "NR_DURACAO")
+    private Duration duracao;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "TP_CATEGOR")
+    private CategoriaFilmeEnum categoria;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "TP_CLASSIFICACAO")
+    private ClassificacaoIndicativaEnum classificacao;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "CHK_EM_CARTAZ")
+    private SimNaoEnum emCartaz;
+
+    @Column(name = "NR_ANO")
+    private Integer ano;
+
+    @Column(name = "TX_CAPA")
+    private String capa;
+
+    @Column(name = "TX_DIRETOR")
+    private String diretor;
+
+    @Column(name = "TX_ELENCO")
+    private String elenco;
+
+    @Column(name = "TX_DESCRICAO")
+    private String descricao;
+
+    @Column(name = "NR_AVALIACAO")
+    private Double avaliacao;
+}
